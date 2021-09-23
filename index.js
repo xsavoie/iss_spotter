1278c4d20641895622602c4b156cf97e6b90e1e2
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-23 21:30:18 +0000

    Created 'callback hell' function to make 3 api calls

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -18,13 +17,29 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 //   console.log('It worked! Returned coordinates:' , coordinates);
 // });
 
-let coords = { latitude: '49.27670', longitude: '-123.13000' }
-// let coords = {}
+// let coords = { latitude: '49.27670', longitude: '-123.13000' };
+// let coords = {};
 
-fetchISSFlyOverTimes(coords, (error, results) =>{
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+// fetchISSFlyOverTimes(coords, (error, results) =>{
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times: ', results)
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log('It worked! Returned flyover times: ', results)
-})
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
--- iss.js
+++ iss.js
@@ -1,15 +1,7 @@
 const request = require('request');
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 
 const fetchMyIP = function(callback) {
-  request("https://api.ipify.org/?format=json",(error, response, body) => {
+  request("https://api.ipify.org/?format=json", (error, response, body) => {
     if (error) return callback(error, null);
 
     if (response.statusCode !== 200) {
@@ -36,28 +28,47 @@ const fetchCoordsByIP = function(ip, callback) {
     const { latitude, longitude } = JSON.parse(body);
 
     callback(null, { latitude, longitude });
-  })
-}
-
-const fetchISSFlyOverTimes = function(coords, callback) {
-  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.latitude}`
-  request(url, (error, response, body) =>{
-  if (error) {
-    callback(error, null);
-    return;
-  }   
-  if (response.statusCode !== 200) {
-    callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
-    return;
-  }
-  const output = (JSON.parse(body))
-    callback(null, output.response)
-  })
+  });
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.latitude}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+    const output = (JSON.parse(body));
+    callback(null, output.response);
+  });
+};
 
-module.exports = { 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchISSFlyOverTimes(coordinates, (error, results) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, results);
+      });
+    });
+  });
+};
+
+module.exports = {
   fetchMyIP,
   fetchCoordsByIP,
-  fetchISSFlyOverTimes
- };
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
+};
